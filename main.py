d1c213a791e3801ea00871daacb2a49ee75f986b
David Zhu <63690620+davidzhuuwa@users.noreply.github.com>
2021-07-23 07:31:41 +0000

    Moved condition for invalid player input to be put upfront before any ASCII is printed

--- main.py
+++ main.py
@@ -32,20 +32,21 @@ import rps
 player_choice = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors\n')
 computer_choice = random.randint(0,2)
 player_choice = int(player_choice)
-print('\n')
-rps.rps_print(player_choice,rock,paper,scissors)
-print('\n')
-print('Computer chose: \n')
-rps.rps_print(computer_choice,rock,paper,scissors)
-
-if player_choice == computer_choice:
-  print('It\'s a draw!')
-elif (player_choice == 0 and computer_choice == 1) or (player_choice == 1 and computer_choice == 2) or (player_choice == 2 and computer_choice == 0):
-  print('You lose')
-elif player_choice >2 or player_choice <0:
+if player_choice >2 or player_choice <0:
   print('You chose an invalid number: you lose!')
 else:
-  print('You win!')
+  print('\n')
+  rps.rps_print(player_choice,rock,paper,scissors)
+  print('\n')
+  print('Computer chose: \n')
+  rps.rps_print(computer_choice,rock,paper,scissors)
+
+  if player_choice == computer_choice:
+    print('It\'s a draw!')
+  elif (player_choice == 0 and computer_choice == 1) or (player_choice == 1 and computer_choice == 2) or (player_choice == 2 and computer_choice == 0):
+    print('You lose')
+  else:
+    print('You win!')
 
 
 
