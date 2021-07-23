2361b39f3259bca2db00cd49c73dcbae0021d22c
David Zhu <63690620+davidzhuuwa@users.noreply.github.com>
2021-07-23 15:21:45 +0800

    Fixed bug where player ASCII art wasn't being printed
    Input from player choice needs to be converted to int for rps function to work correctly

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ import rps
 
 player_choice = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors\n')
 computer_choice = random.randint(0,2)
-
+player_choice = int(player_choice)
 print('\n')
 rps.rps_print(player_choice,rock,paper,scissors)
 print('\n')
